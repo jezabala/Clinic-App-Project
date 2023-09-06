8750f4abffa861f6bed554d69004cdb47d0ffe54
Jesus Zabala <jesusezabala11@gmail.com>
2023-09-05 22:35:35 -0400

    Diseño de pantalla principal, falta colocar un color

--- app/src/main/java/com/jesusdev/clinicaapp/MainActivity.kt
+++ app/src/main/java/com/jesusdev/clinicaapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.jesusdev.clinicaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var directNoticias:CardView
+    private lateinit var directServicios:CardView
+    private lateinit var directMedicos:CardView
+    private lateinit var directContacto:CardView
+    private lateinit var btnLogin:AppCompatButton
+    private lateinit var btnRecuperar:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponents()
+    }
+
+    private fun initComponents(){
+        directNoticias = findViewById(R.id.directNoticias)
+        directServicios = findViewById(R.id.directServicios)
+        directMedicos = findViewById(R.id.directMedicos)
+        directContacto = findViewById(R.id.directContacto)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnRecuperar = findViewById(R.id.btnRecuperarContraseña)
     }
 }
