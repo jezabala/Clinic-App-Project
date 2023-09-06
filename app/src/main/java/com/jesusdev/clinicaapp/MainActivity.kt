d84bc42e738132e57f7f06a06c6a2d164a4a4223
Jesus Zabala <jesusezabala11@gmail.com>
2023-09-06 13:20:16 -0400

    Añadiendo binding para navegacion entre vistas y añadiendo el evento click.

--- app/src/main/java/com/jesusdev/clinicaapp/MainActivity.kt
+++ app/src/main/java/com/jesusdev/clinicaapp/MainActivity.kt
@@ -5,28 +5,23 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.cardview.widget.CardView
+import com.jesusdev.clinicaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var directNoticias:CardView
-    private lateinit var directServicios:CardView
-    private lateinit var directMedicos:CardView
-    private lateinit var directContacto:CardView
-    private lateinit var btnLogin:AppCompatButton
-    private lateinit var btnRecuperar:TextView
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        initComponents()
+        binding.directNoticias.setOnClickListener {  }
+        binding.directServicios.setOnClickListener {  }
+        binding.directMedicos.setOnClickListener {  }
+        binding.directContacto.setOnClickListener {  }
+        binding.btnLogin.setOnClickListener {  }
+
     }
 
-    private fun initComponents(){
-        directNoticias = findViewById(R.id.directNoticias)
-        directServicios = findViewById(R.id.directServicios)
-        directMedicos = findViewById(R.id.directMedicos)
-        directContacto = findViewById(R.id.directContacto)
-        btnLogin = findViewById(R.id.btnLogin)
-        btnRecuperar = findViewById(R.id.btnRecuperarContraseña)
-    }
 }
