033199216fc3bd85b93b3d9589e6da7a92ee53e1
Jesus Zabala <jesusezabala11@gmail.com>
2023-10-10 21:21:17 -0400

    Creacion de tabla base de datos

--- app/src/main/java/com/jesusdev/clinicaapp/AdminSQLiteOpenHelper.kt
+++ app/src/main/java/com/jesusdev/clinicaapp/AdminSQLiteOpenHelper.kt
@@ -1,18 +1,44 @@
 package com.jesusdev.clinicaapp
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 
-class AdminSQLiteOpenHelper(context: Context,
-                            name: String?,
-                            factory: SQLiteDatabase.CursorFactory?,
-                            version: Int): SQLiteOpenHelper(context, name, factory, version) {
+class AdminSQLiteOpenHelper(private val context: Context): SQLiteOpenHelper(context, "clinic_app", null, 1){
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+        db!!.execSQL("CREATE TABLE usuarios(user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, " +
+                "user_email TEXT, user_password TEXT, user_gender TEXT, user_rol TEXT)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
     }
+
+    private fun cerrandoBD(db:SQLiteDatabase){
+        //db.close()
+    }
+
+    fun newUser(username: String, email: String, password: String, gender: String, rol: String){
+        val db = this.writableDatabase
+        try {
+            db.beginTransaction()
+            val datos = ContentValues()
+            datos.put("Usuario", username)
+            datos.put("Correo", email)
+            datos.put("Contraseña", password)
+            datos.put("Genero", gender)
+            datos.put("Rol", rol)
+
+            db.insert("usuarios", null, datos)
+            db.setTransactionSuccessful()
+        } catch (e:java.lang.Exception){
+            e.printStackTrace()
+        } finally {
+            db.endTransaction()
+            cerrandoBD(db)
+        }
+    }
+
 }
--- app/src/main/java/com/jesusdev/clinicaapp/SignUpActivity.kt
+++ app/src/main/java/com/jesusdev/clinicaapp/SignUpActivity.kt
@@ -1,23 +1,41 @@
 package com.jesusdev.clinicaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.jesusdev.clinicaapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var db: AdminSQLiteOpenHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.etName
-        binding.etFechaNacimiento
-        binding.etIdentificacion
-        binding.etTelefono
-        binding.rgGender.checkedRadioButtonId
+        db = AdminSQLiteOpenHelper(this)
+
+        val username = binding.etName
+        val DoB = binding.etFechaNacimiento
+        val cedulaIdentidad = binding.etIdentificacion
+        val telefono = binding.etTelefono
+        val genero = binding.rgGender.checkedRadioButtonId
 
         binding.btnSignUp.setOnClickListener {  }
 
+    }
+
+    private fun signUpDatabase(username: String, email: String, password: String,
+                               gender: String, rol: String){
+        val insertRowId = db.newUser(username, email, password, gender, rol)
+        /*if (insertRowId){
+            Toast.makeText(this, "Registro completado!!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Registro Fallido", Toast.LENGTH_SHORT)
+        } */
     }
 }
